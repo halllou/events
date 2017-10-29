32e96b34789ae5e7ed936d4714720ab77315cc75
Your Name <you@example.com>
2017-10-29 17:25:29 +0000

    Third commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,7 @@
 <script type="text/javascript">
-  $(document).ready(function(){
-    $('.testimonial-slider').slick({
-      setting-name: setting-value
+    $(document).ready(function(){
+      $('.testimonial-slider').slick({
+        setting-name: setting-value
+      });
     });
-  });
-</script>
+  </script>
