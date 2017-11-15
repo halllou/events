60e4d10bdd25360b9884beb66b727fc9474cd159
Your Name <you@example.com>
2017-11-15 19:52:51 +0000

    Sixteenth commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,3 +5,26 @@ var flkty = new Flickity( '.main-gallery', {
   prevNextButtons: false,
   autoPlay: 5000
 });
+
+
+/* Menu collapse */
+
+  $(document).ready(function(){
+	// class of the button in brackets
+   $(".dropbtn").click(function(){
+	   // inside the function here the class of the content to show then css to change
+	   // you can also add other functions but we'll go through in next meet
+       $(".dropdown-content").css("display","block");
+   });
+
+});
+
+</script>
+
+<style type="text/css">
+/* class to be hidden in css */
+.dropdown-content
+{
+display:none;
+}
+</style>
