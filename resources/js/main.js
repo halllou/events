affb116fbd2772046d5b263932a18e1b3af95763
Your Name <you@example.com>
2017-11-15 20:08:12 +0000

    eighteenth commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,4 @@
-/* Menu collapse */
-
-  $(document).ready(function(){
-	// class of the button in brackets
-   $(".dropbtn").click(function(){
-	   // inside the function here the class of the content to show then css to change
-	   // you can also add other functions but we'll go through in next meet
-       $(".dropdown-content").css("display","block");
-   });
-
-});
-
-var flkty = new Flickity( '.main-gallery', {
+var flkty = new Flickity(".main-gallery"){
   cellAlign: 'left',
   contain: true,
   wrapAround: true,
